e148d41d9ea889baee0eaa73392de7c1321e1f2b
Azrod <nabil.natsu@gmail.com>
2019-08-01 18:09:09 +0100

    added urban command and updated !help

--- bot_test.py
+++ bot_test.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from secret import token,dcteam_role_id,dcteam_id
 from utils.tools import get_command_input
+from utils.urban import get_top_def
 
 bot = commands.Bot(command_prefix='!',help_command=None, description=None)
 client=discord.Client()
@@ -20,6 +21,7 @@ async def help(ctx):
     embed.add_field(name="help",value="affiche la liste des commandes",inline=False)
     embed.add_field(name="team",value="assigne le rôle DCTeam au(x) membre(s) mentionné(s)",inline=False)
     embed.add_field(name="getcomics",value="recherche dans getcomics les mots-clés entrés",inline=False)
+    embed.add_field(name="urban",value="fait une recherche du mot éntré sur Urban Dictionary",inline=False)
     await ctx.send(embed=embed)
 
 @bot.command()
@@ -36,4 +38,13 @@ async def team(ctx):
 async def getcomics(ctx):
     user_input = get_command_input(ctx.message.content)
     await ctx.send(f"https://getcomics.info/?s={user_input.lower().replace(' ', '+')}")
+
+@bot.command()
+async def urban(ctx):
+    user_input = get_command_input(ctx.message.content)
+    title,meaning,example,search_url = get_top_def(user_input)
+    embed = discord.Embed(title=f"Definition of {title}",description = meaning, color=0x00FFFF,url=search_url )
+    embed.add_field(name="Example",value=example,inline=False)
+    embed.set_thumbnail(url="https://images-ext-2.discordapp.net/external/HMmIAukJm0YaGc2BKYGx5MuDJw8LUbwqZM9BW9oey5I/https/i.imgur.com/VFXr0ID.jpg")
+    await ctx.send(embed=embed)
 bot.run(token)
---
+++ utils/urban.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import requests  # lib for going on internet
+from bs4 import BeautifulSoup  # html parser
+def get_top_def(user_input):
+
+    urban_url = "https://www.urbandictionary.com/define.php?term={}"
+
+    # Make the search url : www.urbandictionary.com/define.php?term=taco+hole
+    search_url = urban_url.format(user_input.lower().replace(' ', '+'))
+
+    # print(search_url)
+
+    # get HTML page with requests.get
+    res = requests.get(search_url)
+    res.close()
+
+    # BeautifulSoup will transform raw HTML in a tree easy to parse
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    # unquote this if you want to see what is a "soup"
+    # print(soup.prettify())
+
+    # parse the HTML soup to find Top Definition title, meaning, example
+    title = soup.select_one('div.def-panel > div.def-header > a.word').text
+    meaning = soup.select_one('div.def-panel > div.meaning').text
+    example = soup.select_one('div.def-panel > div.example').text
+
+    return title,meaning,example,search_url
