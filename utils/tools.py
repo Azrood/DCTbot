e89d89b331711b93323214f8e15417ed63c1a573
Sergeileduc <sergei.leduc@gmail.com>
2019-08-04 11:49:00 +0200

    Urban_search class object

--- utils/tools.py
+++ utils/tools.py
@@ -1,11 +1,24 @@
 # File for some tools
 
+import requests  # lib for going on internet
+from bs4 import BeautifulSoup
+
 
 def get_command_input(user_input):
     return user_input.split(' ', 1)[1]
+
+
 def string_is_int(string):
     try:
         a = int(string)
         return True
     except ValueError:
         return False
+
+
+def get_soup_lxml(url):
+    # get HTML page with requests.get
+    res = requests.get(url)
+    res.close()
+    # BeautifulSoup will transform raw HTML in a tree easy to parse
+    return BeautifulSoup(res.text, 'lxml')
--- utils/urban.py
+++ utils/urban.py
@@ -1,34 +1,42 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import requests  # lib for going on internet
-from bs4 import BeautifulSoup  # html parser
-import urllib.parse
-def get_top_def(user_input):
+from urllib.parse import quote_plus
 
+from utils.tools import get_soup_lxml
+
+
+# Class to make request on Urban Dictionary
+class Urban_search:
     urban_url = "https://www.urbandictionary.com/define.php?term="
-    formated_input = urllib.parse.quote_plus(user_input.lower(), safe='', encoding=None, errors=None)
-    # Make the search url : www.urbandictionary.com/define.php?term=taco+hole
-    search_url = urban_url+formated_input
 
-    # print(search_url)
+    # init is executed when the object is created
+    def __init__(self, user_input):
+        formated_input = quote_plus(user_input.lower(), safe='', encoding=None,
+                                    errors=None)
 
-    # get HTML page with requests.get
-    res = requests.get(search_url)
-    res.close()
+        # Make search url: www.urbandictionary.com/define.php?term=taco+hole
+        self.search_url = self.urban_url + formated_input
+        self.soup = get_soup_lxml(self.search_url)
+        self.isokay()
 
-    # BeautifulSoup will transform raw HTML in a tree easy to parse
-    soup = BeautifulSoup(res.text, 'lxml')
+    # Test if the page is a definition, or is dummy
+    def isokay(self):
+        # Test if I can find the div for :
+        # Sorry, we couldn't find XXX
+        # If not, search is valid
+        if self.soup.select_one('div.term.space'):
+            self.valid = False
+        else:
+            self.valid = True
 
-    # unquote this if you want to see what is a "soup"
-    # print(soup.prettify())
-
-    # parse the HTML soup to find Top Definition title, meaning, example
-    title = soup.select_one('div.def-panel > div.def-header > a.word').text
-    meaning = soup.select_one('div.def-panel > div.meaning').text
-    example_raw = soup.select_one('div.def-panel > div.example').text
-    if  hasattr(example_raw, 'text'):
-        example = example_raw.text
-    else:
-        example = ""
-    return title,meaning,example,search_url
+    def get_top_def(self):
+        # parse the HTML soup to find Top Definition title, meaning, example
+        title = self.soup.select_one('div.def-panel > div.def-header > a.word').text
+        meaning = self.soup.select_one('div.def-panel > div.meaning').text
+        example_raw = self.soup.select_one('div.def-panel > div.example').text
+        if hasattr(example_raw, 'text'):
+            example = example_raw.text
+        else:
+            example = ""
+        return title, meaning, example, self.search_url
