b2fd1939ba13f1cdb4d345936995a1a29cb88680
Sergeileduc <sergei.leduc@gmail.com>
2019-09-26 17:59:40 +0200

    Docstring comicsblog

--- utils/comicsblog.py
+++ utils/comicsblog.py
@@ -1,7 +1,18 @@
+"""Module to get news on comicsblog.fr."""
+
 from utils.tools import get_soup_html
 
 
 def get_comicsblog(numb):
+    """Get latest news on comicsblog.fr.
+
+    Args:
+        numb (int): number of news to retrieve
+
+    Returns:
+        list: list of comicsblog news
+
+    """
     num = int(numb)
     c_blog_rss = "http://www.comicsblog.fr/comicsblog.rss"
 
