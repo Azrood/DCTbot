acfae20e1d44db1dafe4d13180927220451c5df6
Sergeileduc <sergei.leduc@gmail.com>
2019-09-26 17:39:32 +0200

    Private methods in GifJson

--- utils/gif_json.py
+++ utils/gif_json.py
@@ -1,10 +1,13 @@
+"""Module to handle !gif command (gifs are stored in json file)."""
 import json
 
 
 class GifJson:
+    """Class to handle reading gifs url from json file."""
 
     # init is executed when the object is created
     def __init__(self, file):
+        """Init object with json file, and read it."""
         self.file = file
         # if file exists
         try:
@@ -18,36 +21,39 @@ class GifJson:
                 json.dump(init, outfile)
 
         # read
-        self.file_read()
+        self._file_read()
 
-    def file_read(self):
+    def _file_read(self):
         with open(self.file) as json_file:
             data = json.load(json_file)
         self.gifs = data
         return data
 
-    def file_write(self, data):
+    def _file_write(self, data):
         with open(self.file, 'w') as outfile:
             json.dump(data, outfile, sort_keys=True, indent=4)
 
-    def update_file(self):
+    def _update_file(self):
         with open(self.file, 'w') as outfile:
             json.dump(self.gifs, outfile, sort_keys=True, indent=4)
 
     def gif_delete(self, name):
+        """Delete gif entry and update file."""
         try:
             self.gifs.pop(name)
-            self.update_file()
+            self._update_file()
         except KeyError:
             pass
 
     def gif_add(self, name, url, public=True):
+        """Add gif entry and update file."""
         new_gif = {name.lower(): {'url': url, 'public': public}}
-        self.file_read()
+        self._file_read()
         self.gifs.update(new_gif)
-        self.file_write(self.gifs)
+        self._file_write(self.gifs)
 
     def get_gif(self, name):
+        """Get gif corresponding to 'name'."""
         try:
             return self.gifs[name]
         except KeyError:
