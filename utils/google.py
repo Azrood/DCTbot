8b64cad3fd2c8c8ff792a44c72adb4307244a27d
Sergeileduc <sergei.leduc@gmail.com>
2019-09-26 17:57:38 +0200

    Docstrings in Google

--- utils/google.py
+++ utils/google.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Module to search on Google."""
 
 # import os
 import requests
@@ -8,14 +9,15 @@ from utils.secret import token_youtube, DEVELOPER_CX
 
 
 def search_google(user_input, number):
-    """Search on Google
-    
+    """Search on Google.
+
     Args:
         user_input (str): users's search on Google
         number (int): number of responses
-    
+
     Returns:
         list: list of results (list of dicts with 'title' and 'link' keys)
+
     """
     google_search_url = "https://www.googleapis.com/customsearch/v1"
 
@@ -46,13 +48,14 @@ def search_google(user_input, number):
 
 
 def google_top_link(user_input):
-    """Get first result of Google search
-    
+    """Get first result of Google search.
+
     Args:
         user_input (str): users's search on Google
-    
+
     Returns:
         dict: dict with keys 'title' and 'link'
+
     """
     try:
         result = search_google(user_input, number=1)
