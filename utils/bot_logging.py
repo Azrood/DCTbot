a24d163420a96e56d01cf9cc86e519aaa9fdd1ac
Sergeileduc <sergei.leduc@gmail.com>
2020-01-22 10:18:28 +0100

    logging module

---
+++ utils/bot_logging.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Logging configuration."""
+
+import os
+import json
+import logging
+import logging.config
+
+
+def setup_logging(
+    default_path='logging.json',
+    default_level=logging.INFO,
+    env_key='LOG_CFG'
+):
+    """Setup logging configuration
+
+    """
+    path = default_path
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
