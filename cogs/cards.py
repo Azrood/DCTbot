5df29d4e4434dd76c41bbe8dcbad79fcdcdaef71
Sergeileduc <sergei.leduc@gmail.com>
2022-11-09 16:11:20 +0100

    cards cog : hybrid command

--- cogs/cards.py
+++ cogs/cards.py
@@ -13,8 +13,8 @@ class Cards(commands.Cog):
         self.help = "azrod\nbane\nrun\nsergei\nxanatos\nphoe"
         self.bot = bot
 
-    @commands.command()
-    async def poke(self, ctx, people):
+    @commands.hybrid_command()
+    async def poke(self, ctx, people: str):
         """Send card made by Slyrax."""
         people = people.lower()
         if people == "help":  # probably needs improvements
@@ -24,7 +24,7 @@ class Cards(commands.Cog):
 
         else:
             dir_path = os.path.dirname(os.path.realpath(__file__))
-            card_file = os.path.join(dir_path, os.pardir, f"pictures/cards/{people}.jpg")  # noqa: E501
+            card_file = os.path.join(dir_path, os.pardir, f"pictures/cards/{people}.jpg")  # TODO : change it for Pathlib # noqa: E501
             f = discord.File(fp=card_file, filename=people+".jpg")  # discord.File can't handle f-strings apparently  # noqa: E501,E226
             embed = discord.Embed()
             embed.set_image(url="attachment://"+people+".jpg")  # better safe than sorry  # noqa: E501,E226
