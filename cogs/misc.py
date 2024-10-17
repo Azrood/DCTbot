15f05668aa4c3c43b2fd045d06c16c4566ec114e
Sergeileduc <sergei.leduc@gmail.com>
2024-10-17 10:47:27 +0200

    [LINT] in misc. cog.

--- cogs/misc.py
+++ cogs/misc.py
@@ -10,14 +10,16 @@ import random
 import discord
 from discord.ext import commands
 
-dctradlogo = "http://www.dctrad.fr/ext/planetstyles/flightdeck/store/logodctweb.png"  # noqa: E501
-dctrad_recru = "http://www.dctrad.fr/viewforum.php?f=21"
-snap_url = "https://media.tenor.com/images/8d7d2e757f934793bb4154cede8a4afa/tenor.gif"  # noqa: E501
+DCTRADLOGO = "http://www.dctrad.fr/ext/planetstyles/flightdeck/store/logodctweb.png"  # noqa: E501
+DCTRAD_RECRU = "http://www.dctrad.fr/viewforum.php?f=21"
+SNAP_URL = "https://media.tenor.com/images/8d7d2e757f934793bb4154cede8a4afa/tenor.gif"  # noqa: E501
 
 logger = logging.getLogger(__name__)
 
 
 class Misc(commands.Cog):
+    "Cog for various commands"
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -26,8 +28,8 @@ class Misc(commands.Cog):
         """Send 'recrutement' topic url."""
         embed = discord.Embed(title="Rejoins la team DCTrad !",
                               description="allez n'aie pas peur de cliquer et deviens un héros !",  # noqa: E501
-                              color=0x0000FF, url=dctrad_recru)
-        embed.set_thumbnail(url=dctradlogo)
+                              color=0x0000FF, url=DCTRAD_RECRU)
+        embed.set_thumbnail(url=DCTRADLOGO)
         await ctx.send(embed=embed)
 
     @commands.hybrid_command()
@@ -40,7 +42,7 @@ class Misc(commands.Cog):
         """
         await ctx.send(content=f"{ctx.author.mention} : timer enregistré !",
                        delete_after=10)
-        await asyncio.sleep(num, result=None, loop=None)
+        await asyncio.sleep(num, result=None)
         await ctx.send(content=f"temps écoulé ! : {ctx.author.mention} {args}")
 
     @commands.command()
@@ -71,7 +73,7 @@ class Misc(commands.Cog):
     async def ping(self, ctx):
         """Ping the bot."""
         await ctx.send(content="pong !")
-        logger.info(f"Ping (asked by {ctx.author}) was awaited.")
+        logger.info("Ping (asked by %s) was awaited.", ctx.author)
 
     @commands.hybrid_command()
     async def roulette(self, ctx: commands.Context):
@@ -87,8 +89,8 @@ class Misc(commands.Cog):
                                                   "Ca va péter !",
                                                   "Il va faire nuit !"]))
 
-            await ctx.send(content=snap_url, delete_after=4)
-            await asyncio.sleep(2.4, result=None, loop=None)
+            await ctx.send(content=SNAP_URL, delete_after=4)
+            await asyncio.sleep(2.4)
             await ctx.author.kick()
         else:
             close = random.choice([
